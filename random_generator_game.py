2ff7bf9309ae3f3ef3d07622bae526ce32e9ceb2
Joel Handler <jhandler@htps.us>
2015-07-06 13:45:12 -0400

    Added some code

--- random_generator_game.py
+++ random_generator_game.py
@@ -1,3 +1,14 @@
 import random
+
 num = random.randint(1,10)
-print num
+
+print "The computer Picked: %d" % (num)
+
+userinput = input("Guess a Number: ")
+
+print "You chose %d" % (userinput)
+
+if (num == abs(userinput)):
+	print "Yeah"
+else:
+	print "no"
